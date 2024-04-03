c6335b6ba50d28277a02aa6be46da00f7eeec2be
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-04-03 15:10:18 -0500

    Auto Array HW

--- mercurySedan.js
+++ mercurySedan.js
@@ -13,11 +13,19 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends VehicleModule.Vehicle {
+    constructor(make, model, year, color, mileage, numDoors) {
+        super(make, model, year, color, mileage);
+        this.numDoors = numDoors;
+
+    }
+}
 
 
+module.exports = {
+    Car: Car
 
-
-
+};
 
 
 
@@ -28,8 +36,20 @@ console.log(v.make)
 
 //You can use the same instance "v" of the Vehicle class above for the base class.
 
+const CarModule = require("./carClass");
+let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+console.log(v.make)
 
 
 
 
 //Create at least two new instances of the Car class and test them here:
+
+let car1 = new CarModule.Car("Toyota", "Camry", "2020", "red", "20000", 4);
+let car2 = new CarModule.Car("Honda", "Accord", "2019", "blue", "25000", 4);
+
+
+console.log(car1.make); // Output: Toyota
+console.log(car2.model); // Output: Accord
+console.log(car1.numDoors); // Output: 4
+console.log(car2.color); // Output: blue
